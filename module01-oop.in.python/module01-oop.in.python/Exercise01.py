14e3828a90b9d6970cf47ccc91f96b74ccd5fad4
Binnur KURT <deepcloudlabs@gmail.com>
2024-10-07 14:26:46 +0300

    Update Exercise01.py

--- module01-oop.in.python/module01-oop.in.python/Exercise01.py
+++ module01-oop.in.python/module01-oop.in.python/Exercise01.py
@@ -53,6 +53,10 @@ class Account:
         return self.__balance
 
     @property
+    def iban(self):
+        return self.__iban
+
+    @property
     def status(self):
         return self.__status
 
@@ -68,10 +72,53 @@ class Account:
         return f"Account: iban: {self.__iban}, balance: {self.__balance}, status: {self.__status}"
 
 
+"""
+CheckingAccount: sub class,derived class
+Account        : super class, base class
+"""
+
+
+class CheckingAccount(Account):
+    def __init__(self, iban, balance=5_000, status=AccountStatus.ACTIVE, overdraft_amount=1_000):
+        super().__init__(iban, balance, status)
+        self.__overdraftAmount = overdraft_amount
+
+    # overriding
+    def withdraw(self, amount):
+        if self.__status != AccountStatus.ACTIVE:
+            raise ValueError('Account is not active')
+        # validation rule
+        if amount <= 0.0:
+            raise ValueError('Amount must be positive')
+        # business rule
+        if amount > (self.__balance + self.__overdraftAmount):
+            deficit = amount - self.__balance - self.__overdraftAmount
+            # business exception
+            raise InsufficientBalanceException("Your balance does not cover your expenses", deficit)
+        self.__balance = self.__balance - amount
+        return self.__balance
+
+    @property
+    def overdraft_balance(self):
+        return self.__overdraftAmount
+
+    @overdraft_balance.setter
+    def overdraft_balance(self, overdraft_amount):
+        if overdraft_amount <= 0.0:
+            raise ValueError('Overdraft amount must be positive')
+        self.__overdraftAmount = overdraft_amount
+
+    def __str__(self):
+        return f"CheckingAccount: iban: {self.iban}, balance: {self.balance}, status: {self.status}, overdraftAmount: {self.__overdraftAmount}"
+
+
 acc1 = Account("TR290006222359813456984831", 100_000)
+acc2 = CheckingAccount("TR290006222359813456984831", 100_000, overdraft_amount=10_000)
+
 try:
     acc1.withdraw(50_000)
     print(acc1)
+    print(acc2)
     acc1.withdraw(25_000)
     print(str(acc1))
     print(acc1)
