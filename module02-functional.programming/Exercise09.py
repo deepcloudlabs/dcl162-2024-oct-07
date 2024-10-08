b84fec2ab40ffca4c001611e83107fa0c2adb169
Binnur KURT <deepcloudlabs@gmail.com>
2024-10-08 16:34:06 +0300

    Update Exercise09.py

--- module02-functional.programming/Exercise09.py
+++ module02-functional.programming/Exercise09.py
@@ -1,5 +1,19 @@
 import json
+from functools import reduce
+from itertools import chain
+
+
+def genreHistogramReducer(histogram, genre):
+    histogram[genre] = histogram.get(genre, 0) + 1
+    return histogram
+
 
 with open("resources/movies.json", "rt") as file:
     movies = json.load(file)
-    # histogram -> genre
+    # flattening: chain.from_iterable
+    movieGenreHistogram = reduce(genreHistogramReducer, chain.from_iterable(filter(lambda genres: len(genres) > 0,
+                                                                                   map(lambda movie: [genre["name"] for
+                                                                                                      genre in
+                                                                                                      movie["genres"]],
+                                                                                       movies))), {})
+    print(movieGenreHistogram)
